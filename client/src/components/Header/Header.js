e2b511bd0611d560dc4e5ac5d1ad35b7825138c9
Hieu Lai <hieuqlai@gmail.com>
2019-03-24 13:56:01 +1100

    header solid color when not at top

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Wrapper } from './Header.sc'
 import Brand from '../Brand/Brand'
 import { ContentContainer } from '../../styles/Containers.sc'
 import Menu from '../Menu/Menu'
 
-const Header = () => (
-  <Container>
-    <ContentContainer>
-      <Wrapper>
-        <Brand />
-        <Menu />
-      </Wrapper>
-    </ContentContainer>
-  </Container>
-)
+const Header = () => {
+  const [isTop, setIsTop] = useState(true)
+
+  useEffect(() => {
+    if (window.scrollY === 0) {
+      setIsTop(true)
+    } else {
+      setIsTop(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY === 0) {
+        setIsTop(true)
+      } else {
+        setIsTop(false)
+      }
+    })
+  })
+
+  return (
+    <Container isTop={isTop}>
+      <ContentContainer>
+        <Wrapper>
+          <Brand />
+          <Menu />
+        </Wrapper>
+      </ContentContainer>
+    </Container>
+  )
+}
 
 export default Header
--- client/src/components/Header/Header.sc.js
+++ client/src/components/Header/Header.sc.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding: 2rem 0;
+  ${props => !props.isTop && `
+    background: #2f5af7;
+    box-shadow: 0 0 10px rgba(0,0,0,0.3);
+  `};
+  padding: 2rem 0; 
   position: fixed;
   width: 100%;
+  transition: all 1s;
 `
 
 export const Wrapper = styled.div`
