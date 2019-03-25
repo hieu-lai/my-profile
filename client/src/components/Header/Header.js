be3ab9efd9b0482a6fad4126314b3a1efc593115
Hieu Lai <hieulai@Hieus-MacBook-Pro.local>
2019-03-25 12:42:57 +1100

    lottie animate on click

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -30,6 +30,7 @@ const Header = () => {
       <ContentContainer>
         <Wrapper>
           <Brand />
+          {/* <Menu /> */}
           <MemoComponent />
         </Wrapper>
       </ContentContainer>
--- client/src/components/Lottie/Switch.js
+++ client/src/components/Lottie/Switch.js
@@ -4,70 +4,35 @@ import * as animationData from '../../lottie/switch.json'
 import { LottieWrapper, Clickable } from './Shared.sc.js'
 
 const Switch = () => {
-  // const [isStopped, setIsStopped] = useState(true)
   const [isNightMode, setIsNightMode] = useState(false)
-  const [isPaused, setIsPaused] = useState(true)
   const lottieRef = useRef(null)
+
+  const onClick = () => {
+    if (isNightMode) {
+      lottieRef.current.anim.playSegments([48, 96], true)
+      setIsNightMode(false)
+    } else {
+      lottieRef.current.anim.playSegments([0, 48], true)
+      setIsNightMode(true)
+    }
+  }
 
   const defaultOptions = {
     loop: false,
     autoplay: false, 
     animationData: animationData.default,
-    direction: 1,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
     }
   }
-
-  const eventListeners=[
-    {
-      eventName: 'enterFrame',
-      callback: (e) => {callbackToNight(e); console.log(isNightMode)},
-    },
-    // {
-    //   eventName: 'complete',
-    //   callback: () => {setIsPaused(true); setIsNightMode(false)}
-    // }
-  ]
-
-  const callbackToNight = (e) => {
-    // console.log('callback')
-    // console.log('isNightMode:', isNightMode, 'isPaused:', isPaused)
-    if (isNightMode) {
-      console.log('here')
-    } else {
-      if (e.currentTime > 48) {
-
-        console.log('here3', isNightMode)
-        setIsPaused(true)
-        setIsNightMode(true)
-      }
-    }
-    
-  }
-
-  // const callbackToDay = (e) => {
-  //   console.log('here2')
-  //   setIsPaused(false)
-  // }
-
-  const onClick = () => {
-    setIsPaused(false)
-    console.log(isNightMode)
-    console.log('clicked')
-  }
-
-  console.log('isNightMode:', isNightMode, 'isPaused:', isPaused)
 
   return (
     <LottieWrapper>
       <Lottie 
         ref={lottieRef}
         options={defaultOptions}
-        eventListeners={eventListeners}
         height={45}
         width={70}
-        isPaused={isPaused}
         isClickToPauseDisabled={true}
       />
       <Clickable onClick={onClick} />
